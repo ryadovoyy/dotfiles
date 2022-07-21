307b33198763e67d8ac7600f634b57a65054f436
ryadovoyy <holstinindvs@gmail.com>
2022-07-21 08:47:19 +0700

    Fix status monitor

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -2,32 +2,32 @@
 
 dwm_lang() {
     case "$(xset -q | grep LED | awk '{ print $10 }')" in
-        "00000000") echo "󰌌 En";;
-        "00001000") echo "󰌌 Ru";;
-                 *) echo "󰌌 Err";;
+        "00000000") echo " En";;
+        "00001000") echo " Ru";;
+                 *) echo " Err";;
     esac
 }
 
-dwm_pulse () {
+dwm_pulse() {
     vol=$(pactl list sinks | grep '^[[:space:]]Volume:' | \
         head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
     if [ "$vol" -eq 0 ]; then
-        echo "󰝟"
+        echo "婢"
     elif [ "$vol" -gt 0 ] && [ "$vol" -le 33 ]; then
-        echo "󰕿$vol"
+        echo "奄 $vol"
     elif [ "$vol" -gt 33 ] && [ "$vol" -le 66 ]; then
-        echo "󰖀$vol"
+        echo "奔 $vol"
     else
-        echo "󰕾$vol"
+        echo "墳 $vol"
     fi
 }
 
-dwm_date () {
-    echo "󰃰 $(date "+%a, %d %b %H:%M")"
+dwm_date() {
+    echo " $(date "+%a, %d %b %H:%M")"
 }
 
-generate_content () {
-    echo "[$(dwm_lang)] [$(dwm_pulse)] [$(dwm_date)]"
+generate_content() {
+    echo "$(dwm_lang)  $(dwm_pulse)  $(dwm_date)"
 }
 
 while true; do
