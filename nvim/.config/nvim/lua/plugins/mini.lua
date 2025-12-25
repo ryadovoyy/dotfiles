8fe973a66673c51ff6849eaeb80c5518e4c48ae4
ryadovoyy <holstinindvs@gmail.com>
2025-12-26 03:29:14 +0700

    Fix nvim mini plugins

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,9 +1,24 @@
 -- text editing
+require('mini.operators').setup({
+  sort = {
+    prefix = 'go',
+  },
+})
+
+require('mini.surround').setup({
+  mappings = {
+    add = 'gsa',
+    delete = 'gsd',
+    find = 'gsf',
+    find_left = 'gsF',
+    highlight = 'gsh',
+    replace = 'gsr',
+  },
+})
+
 require('mini.comment').setup()
-require('mini.operators').setup()
 require('mini.pairs').setup()
 require('mini.splitjoin').setup()
-require('mini.surround').setup()
 
 -- general workflow
 require('mini.bufremove').setup()
